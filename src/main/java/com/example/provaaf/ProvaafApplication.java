916c109270da607f4b620c0d033bb82a7143e0fc
laupontiroli <laurapm@al.insper.edu.br>
2024-11-11 09:51:19 -0300

    base

---
+++ src/main/java/com/example/provaaf/ProvaafApplication.java
@@ -0,0 +1,13 @@
+package com.example.provaaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/provaaf/ProvaafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provaaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
